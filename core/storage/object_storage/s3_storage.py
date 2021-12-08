855ed96d6a0c81309ee82691310407b8a6488baa
Neklyudov Semen <s.neklyudov@mtsai.team>
2021-12-09 01:50:22 +0300

    Add get object method

--- core/storage/object_storage/s3_storage.py
+++ core/storage/object_storage/s3_storage.py
@@ -3,9 +3,8 @@ from aiobotocore.session import AioSession, get_session
 from contextlib import AsyncExitStack
 from base import BaseS3Client, BaseS3Manager
 import asyncio
-import http
 import logging
-from typing import List
+from typing import List, Optional
 
 logger = logging.Logger(name="S3StorageLogger")
 ch = logging.StreamHandler()
@@ -56,18 +55,23 @@ class S3Manager(BaseS3Manager):
             buckets = await client.list_buckets()
             return buckets
 
-    async def list_objects(self, bucket_name: str) -> List[str]:
+    async def list_objects(self, bucket_name: str, prefix: Optional[str] = None) -> List[str]:
         async with self._s3 as client:
             try:
-                objects = await client.list_objects(Bucket=bucket_name)
-                print(objects)
+                objects = await client.list_objects(Bucket=bucket_name, Prefix=prefix)
+                return objects
             except Exception as ex:
                 if "NoSuchBucket" in str(ex):
                     return []
                 logger.exception(ex)
 
-    async def get_object(self, bucket_name: str, object_name: str):
-        pass
+    async def get_object(self, bucket_name: str, object_name: str) -> bytes:
+        async with self._s3 as client:
+            try:
+                s3_object = await client.get_object(Bucket=bucket_name, Key=object_name)
+                return s3_object['Body']
+            except Exception as ex:
+                logger.exception(ex)
 
     async def put_object(self, bucket_name: str, object_name: str, bytes_object: bytes) -> None:
         async with self._s3 as client:
@@ -105,14 +109,3 @@ class S3Manager(BaseS3Manager):
                     logger.warning(msg="Bucket {bucket} already created".format(bucket=bucket_name))
                     return
                 logger.exception(ex)
-
-
-async def client_code():
-    mngr = S3Manager()
-    bytes_file = open('addresses.csv', 'rb')
-    lst = await mngr.put_object("test4", '1235', bytes_file)
-
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(client_code())
